a7d6fc18c76e280bac0e2b0631b152c519090e08
Erick Brandao <brandaoerick@MacBook-Air.local>
2023-02-01 13:19:33 -0300

    first version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [load, setLoad] = useState(0);
+
+  useEffect(() => {
+    const int = setInterval(() => {
+      setLoad((load) => (load > 99 ? clearInterval(int) : load + 1));
+    }, 60);
+    return () => clearInterval(int);
+  }, []);
+
+  const scale = (num, in_min, in_max, out_min, out_max) =>
+    ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+
   return (
     <div className="App">
+      <section
+        className="bg"
+        style={{ filter: `blur(${scale(load, 0, 100, 30, 0)}px)` }}
+      />
+      <div
+        className="loading-text"
+        style={{ opacity: scale(load, 0, 100, 1, 0) }}
+      >
+        {load}%
+      </div>
     </div>
   );
-}
+};
 
 export default App;
